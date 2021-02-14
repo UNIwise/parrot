7c78f7a6b0f8454431c0157d1d93511d5bf33b59
Paul Farver <paulfarver@gmail.com>
2021-02-14 01:28:05 +0100

    Implement cache logic in project language request
    Signed-off-by: Paul Farver <paulfarver@gmail.com>

--- internal/rest/v1/translation.go
+++ internal/rest/v1/translation.go
@@ -1,9 +1,12 @@
 package v1
 
 import (
+	"bytes"
 	"errors"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/uniwise/parrot/internal/cache"
 )
 
 type getProjectLanguageRequest struct {
@@ -11,6 +14,28 @@ type getProjectLanguageRequest struct {
 	Language *string `url:"language" validate:"required"`
 }
 
-func getProjectLanguage(c echo.Context) error {
-	return errors.New("Not implemented")
+func getProjectLanguage(ctx echo.Context) error {
+	c := ctx.(*Context)
+
+	req := new(getProjectLanguageRequest)
+	if err := c.Bind(req); err != nil {
+		c.Log.WithError(err).Error("Error binding request")
+		return echo.ErrBadRequest
+	}
+
+	if err := c.Validate(req); err != nil {
+		c.Log.WithError(err).Error("Error validating request")
+		return echo.ErrBadRequest
+	}
+
+	b, err := c.Cacher.GetTranslation(*req.Project, *req.Language, "key_value_json")
+	if err == nil {
+		return c.Stream(http.StatusOK, "application/json", bytes.NewReader(b))
+	} else if !errors.Is(err, cache.ErrCacheMiss) {
+		c.Log.WithError(err).Error("Error fetching cached data")
+		return echo.ErrInternalServerError
+	}
+	c.Log.Debug("Cache miss!")
+
+	return echo.NewHTTPError(http.StatusNotImplemented, "Not implemented")
 }
