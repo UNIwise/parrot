ab2f0afbfec8435cf48d4083ea2568793228cc25
Jonas Tranberg <jonastranberg93@gmail.com>
2021-04-12 15:44:33 +0200

    Refactor the rest server structure

--- internal/rest/server.go
+++ internal/rest/server.go
@@ -6,12 +6,10 @@ import (
 
 	gosundheit "github.com/AppsFlyer/go-sundheit"
 	healthhttp "github.com/AppsFlyer/go-sundheit/http"
-	"github.com/labstack/gommon/random"
 	"github.com/pkg/errors"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	eprom "github.com/paulfarver/echo-pack/middleware"
 	"github.com/sirupsen/logrus"
 	"github.com/uniwise/parrot/internal/project"
 	v1 "github.com/uniwise/parrot/internal/rest/v1"
@@ -19,45 +17,26 @@ import (
 
 const (
 	gzipCompressionLevel = 5
-	requestIDLength      = 10
 )
 
 type Server struct {
 	Echo *echo.Echo
 }
 
-func NewServer(projectService project.Service, entry *logrus.Entry, enablePrometheus bool) (*Server, error) {
+func NewServer(l *logrus.Entry, projectService project.Service, enablePrometheus bool) (*Server, error) {
 	e := echo.New()
+
 	e.HideBanner = true
 	e.HidePort = true
 	e.Validator = NewValidator()
 
-	// Middleware
 	e.Use(middleware.Recover())
+	e.Use(middleware.RequestID())
 	e.Use(middleware.GzipWithConfig(middleware.GzipConfig{
 		Level: gzipCompressionLevel,
 	}))
-	e.Use(middleware.RequestIDWithConfig(middleware.RequestIDConfig{
-		Generator: func() string {
-			return random.String(requestIDLength, random.Hex)
-		},
-	}))
 
-	v1Group := e.Group("/v1", func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			cc := &v1.Context{
-				Context:        c,
-				ProjectService: projectService,
-				Log:            entry.WithField("requestID", c.Response().Header().Get(echo.HeaderXRequestID)),
-			}
-
-			return next(cc)
-		}
-	})
-	if enablePrometheus {
-		v1Group.Use(eprom.Prometheus())
-	}
-	v1.Register(v1Group)
+	v1.Register(e, l, projectService, enablePrometheus)
 
 	h := gosundheit.New()
 
