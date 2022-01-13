4f5df4190ffcc5e889058329f94ff7f9f7148b69
Paul Farver <paulfarver@gmail.com>
2022-01-13 17:48:10 +0100

    Comment functions to provide documentation
    Signed-off-by: Paul Farver <paulfarver@gmail.com>

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -11,39 +11,47 @@ import (
 	"gopkg.in/resty.v1"
 )
 
+// Client is a client for fetching translations from parrot.
 type Client interface {
+	// GetTranslation returns a map of terms for the given language.
 	GetTranslation(ctx context.Context, language string) (map[string]string, error)
 }
 
+// CachedClientImpl is a client that caches translations in memory.
 type CachedClientImpl struct {
 	httpClient *resty.Client
 	projectID  int
-	cache      map[string]CacheItem
+	cache      map[string]cacheItem
 	mutex      *sync.Mutex
 	ttl        time.Duration
 }
 
+// NewCachedClient creates a new client that caches translations in memory.
 func NewCachedClient(endpoint string, project int, ttl time.Duration) *CachedClientImpl {
 	return &CachedClientImpl{
 		httpClient: resty.New().SetHostURL(endpoint),
 		projectID:  project,
-		cache:      map[string]CacheItem{},
+		cache:      map[string]cacheItem{},
 		mutex:      &sync.Mutex{},
 		ttl:        ttl,
 	}
 }
 
-type CacheItem struct {
+// cacheItem is a single cached translation, as it appears in the cache.
+type cacheItem struct {
 	Etag    string
 	Data    map[string]string
 	Expires time.Time
 }
 
-type Response []struct {
+// response is the response type returned from parrot, with the json format.
+type response []struct {
 	Term       string `json:"term"`
 	Definition string `json:"definition"`
 }
 
+// GetTranslation returns a map of terms for the given language. It will return cached data
+// if it has not expired, otherwise it will fetch the data from the upstream.
 func (c *CachedClientImpl) GetTranslation(ctx context.Context, language string) (map[string]string, error) {
 	key := cacheKey(c.projectID, language)
 	// Lock here to avoid multiple concurrent requests to upstream
@@ -54,6 +62,7 @@ func (c *CachedClientImpl) GetTranslation(ctx context.Context, language string) 
 		return cached.Data, nil
 	}
 
+	// Fetch from upstream and store in the cache
 	request := c.httpClient.R().
 		SetContext(ctx).
 		SetPathParams(map[string]string{
@@ -61,7 +70,7 @@ func (c *CachedClientImpl) GetTranslation(ctx context.Context, language string) 
 			"language": language,
 		}).
 		SetQueryParam("format", "json").
-		SetResult(Response{})
+		SetResult(response{})
 
 	if exists {
 		request = request.SetHeader("If-None-Match", cached.Etag)
@@ -69,13 +78,13 @@ func (c *CachedClientImpl) GetTranslation(ctx context.Context, language string) 
 
 	resp, err := request.Get("/v1/project/{project}/language/{language}")
 	if err != nil {
-		return nil, errors.Wrap(err, "Failed to get translation")
+		return nil, errors.Wrap(err, "Failed to fetch translation")
 	}
 
 	switch resp.StatusCode() {
 	case http.StatusNotModified:
-		// Renew cached item
-		c.cache[key] = CacheItem{
+		// Handle 304 Not Modified by renewing the cached item
+		c.cache[key] = cacheItem{
 			Etag:    cached.Etag,
 			Data:    cached.Data,
 			Expires: time.Now().Add(c.ttl),
@@ -83,13 +92,13 @@ func (c *CachedClientImpl) GetTranslation(ctx context.Context, language string) 
 
 		return cached.Data, nil
 	case http.StatusOK:
-		// Cache response
-		data, ok := resp.Result().(*Response)
+		// Handle 200 OK by storing the response in the cache
+		data, ok := resp.Result().(*response)
 		if !ok || data == nil {
 			return nil, errors.Errorf("Failed to parse response: '%s'", data)
 		}
 		result := mapResponse(*data)
-		c.cache[key] = CacheItem{
+		c.cache[key] = cacheItem{
 			Etag:    resp.Header().Get("Etag"),
 			Data:    result,
 			Expires: time.Now().Add(c.ttl),
@@ -105,7 +114,8 @@ func cacheKey(projectID int, language string) string {
 	return "translation:" + strconv.Itoa(projectID) + ":" + language
 }
 
-func mapResponse(resp Response) map[string]string {
+// mapResponse converts the response from parrot into a map of terms.
+func mapResponse(resp response) map[string]string {
 	result := map[string]string{}
 
 	for _, item := range resp {
