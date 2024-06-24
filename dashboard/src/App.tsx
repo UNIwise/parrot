fa098067dff112021ab95d93a6cac72db4ee19e7
Max Ivanov <gaaraqwerty@gmail.com>
2024-06-24 14:27:06 +0200

    added some files to build further project on top of that

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Box from '@mui/joy/Box';
+import Breadcrumbs from '@mui/joy/Breadcrumbs';
+import CssBaseline from '@mui/joy/CssBaseline';
+import Link from '@mui/joy/Link';
+import { CssVarsProvider } from '@mui/joy/styles';
+import Typography from '@mui/joy/Typography';
 
-function App() {
-  const [count, setCount] = useState(0)
+import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import OrderTable from './components/OrderTable';
+
+export default function JoyOrderDashboardTemplate() {
+    return (
+        <CssVarsProvider disableTransitionOnChange>
+            <CssBaseline />
+            <Box sx={{ display: 'flex', minHeight: '100vh' }}>
+                <Box
+                    component="main"
+                    className="MainContent"
+                    sx={{
+                        px: { xs: 2, md: 6 },
+                        pt: {
+                            xs: 'calc(12px + var(--Header-height))',
+                            sm: 'calc(12px + var(--Header-height))',
+                            md: 3,
+                        },
+                        pb: { xs: 2, sm: 2, md: 3 },
+                        flex: 1,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        minWidth: 0,
+                        height: '100dvh',
+                        gap: 1,
+                    }}
+                >
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        <Breadcrumbs
+                            size="sm"
+                            aria-label="breadcrumbs"
+                            separator={<ChevronRightRoundedIcon fontSize="small" />}
+                            sx={{ pl: 0 }}
+                        >
+                            <Link underline="none" color="neutral" href="#some-link" aria-label="Home">
+                                <HomeRoundedIcon />
+                            </Link>
+
+                            <Link
+                                underline="hover"
+                                color="neutral"
+                                href="#some-link"
+                                fontSize={12}
+                                fontWeight={500}
+                                content="Dashboard"
+                            >
+                                Dashboard
+                            </Link>
+
+                            <Typography color="primary" fontWeight={500} fontSize={12}>
+                                Orders
+                            </Typography>
+                        </Breadcrumbs>
+                    </Box>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            mb: 1,
+                            gap: 1,
+                            flexDirection: { xs: 'column', sm: 'row' },
+                            alignItems: { xs: 'start', sm: 'center' },
+                            flexWrap: 'wrap',
+                            justifyContent: 'space-between',
+                        }}
+                    >
+                        <Typography level="h2" component="h1">
+                            Orders
+                        </Typography>
+                    </Box>
+                    <OrderTable />
+                </Box>
+            </Box>
+        </CssVarsProvider>
+    );
 }
-
-export default App
