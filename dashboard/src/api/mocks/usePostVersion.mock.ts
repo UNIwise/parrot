2b5338a23ef8799e46e088a0667e7b2efe055fad
Max Ivanov <gaaraqwerty@gmail.com>
2024-07-12 19:40:05 +0200

    added type to mocked post data in usePostRequest

--- dashboard/src/api/mocks/usePostVersion.mock.ts
+++ dashboard/src/api/mocks/usePostVersion.mock.ts
@@ -1,15 +1,13 @@
+import { Version } from "../../interfaces/versions";
 import { mock } from "../client";
 import { mockedVersionsResponse } from "./useGetVersions.mock";
 
 mock.onPost(/^\/api\/v1\/projects\/\d+\/versions$/).reply((req) => {
   const request = JSON.parse(req.data);
-  const projectId = parseInt(req.url!.split("/")[4]);
 
-  const newVersion = {
+  const newVersion: Version = {
     id: Math.floor(Math.random() * 1000),
     name: request.name,
-    projectId: projectId,
-    version: request.version,
     createdAt: new Date().toISOString(),
   };
 
