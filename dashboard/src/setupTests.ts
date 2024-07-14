c18bc9566af0025dee2ae439014052c8d9d77954
Max Ivanov <gaaraqwerty@gmail.com>
2024-07-14 15:48:47 +0200

    added vitest/ui, react testing library, jsdom, node types

---
+++ dashboard/src/setupTests.ts
@@ -0,0 +1,13 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import matchers from '@testing-library/jest-dom/matchers';
+import { cleanup } from '@testing-library/react';
+import { afterEach, expect } from 'vitest';
+
+expect.extend(matchers);
+
+afterEach(() => {
+  cleanup();
+});
